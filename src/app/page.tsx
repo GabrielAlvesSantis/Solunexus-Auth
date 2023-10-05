068cc6919e316570873f18a89575860314a92cc2
GabrielAlvesSantis <gabriel.alves.santis@gmail.com>
2023-10-05 10:31:50 -0300

    chore: add page for redefine password and new font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-   <div>
-    <h1>Hello World!</h1>
+   <div className="grid lg:grid-cols-2  md:grid-cols-1 min-h-screen">
+      <div className="flex flex-col justify-center items-center w-full max-h-full bg-slate-950">
+        <h1 className="text-4xl  font-bold font-sans">Solunexus</h1>
+      </div>
+      <div className="flex flex-col justify-center items-center w-full max-h-full gap-5">
+          <div>
+            <h1 className="lg:text-4xl md:text-3xl text-center font-bold font-sans">Redefinição de senha</h1>
+            <p className="text-md  text-center font-sans">Digite seu e-mail para redefinir sua senha</p>
+            <form className="flex flex-col gap-5 mt-5">
+              <input className="p-2 rounded-md border border-gray-400 text-black font-sans" type="text" placeholder="E-mail" />
+              <button className="p-2 rounded-md bg-green-500 text-white font-medium font-sans" type="submit">Enviar</button>
+            </form>
+          </div>
+      </div>
    </div>
   )
 }
